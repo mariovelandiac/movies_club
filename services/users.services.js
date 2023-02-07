5498e97ef7b3c5bb5b426eb5664467f89670e713
mariovelandiac <mariovelandiawork@gmail.com>
2023-02-07 00:31:21 -0500

    endpoint con problema de seguridad corregido

--- services/users.services.js
+++ services/users.services.js
@@ -11,6 +11,9 @@ class UsersService {
   }
 
   async create(data) {
+    if (data.role === 'admin') {
+      throw boom.unauthorized()
+    }
     const hash = await bcrypt.hash(data.password, 10);
     const newUser = await models.User.create({
       ...data,
